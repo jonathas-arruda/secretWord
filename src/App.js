f784c6e8741a51ef04b515d55f7c1d446a2e246c
Jonathas <jonathasarruda@outlook.com>
2022-08-04 21:40:16 -0300

    Create components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import StartScreen from './components/StartScreen';
+import { useCallback, useEffect, useState } from "react";
+
+// styles
+import "./App.css";
+
+// components
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+// data
+import { wordsList } from "./data/words";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage,setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+  console.log(words);
   return (
     <div className="App">
-     <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   );
 }
